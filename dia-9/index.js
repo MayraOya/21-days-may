56159863c6e26b16eb73e5d76befcb92250f6b0e
Mayra Vilalba <mayra.gaudencio@gmail.com>
2024-09-23 19:17:05 -0300

    adicionando exercicios 1#

--- dia-9/index.js
+++ dia-9/index.js
@@ -21,6 +21,38 @@ let meses = idade * 12
 let semanas = idade * 52
 let dias = idade * 365
 
-console.log()
+console.log('Sua idade em meses é ' + meses)
+console.log('Sua idade em semanas é ' + semanas)
+console.log('Sua idade em dias é ' + dias)
 
+//Descobrindo imc
+let imc = parseInt(peso / (altura * altura))
 
+if(imc < 18,5) {
+    console.log('Magreza')
+}else if(imc >= 18,5 && imc <= 24,9) {
+    console.log('Normal')
+}else if(imc >= 24,9 && imc <= 30) {
+    console.log('Sobrepeso')
+}else {
+    console.log('Obesidade')
+}
+
+//Descobrindo o ano Nascimento
+let anoAtual = 2024
+let anoNasc = anoAtual - idade
+console.log('Você nasceu em ' + anoNasc)
+
+let anoVivido = anoNasc
+let idadeAtual = 0
+
+for (let anoVivido = anoNasc; anoVivido <= anoAtual; anoVivido++) {
+    console.log(anoVivido + ' - ' + idadeAtual + ' anos de idade')
+    idadeAtual++
+}
+
+//Loop DoWhile
+do {
+    //Inserir Lógica Anterior
+    continuar = prompt('Deseja inserir os dados novamente? (s/n)')
+} while (continuar == 's')
